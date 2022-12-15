eab4c34c3468b6fd0207bb9e0ace313c70a18ee1
Alla Katkova <allakatkova@bk.ru>
2022-12-16 00:23:26 +0500

    Added completed tasks 1 and 2

---
+++ task_2/task_2_sql_additional_emploeyes.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS Employees (
+	employee_id SERIAL PRIMARY KEY, 
+	name VARCHAR(100) NOT NULL,
+	department VARCHAR(100) NOT NULL,
+	chief_id INTEGER REFERENCES Employees(id));
+	
